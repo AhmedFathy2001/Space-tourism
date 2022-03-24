4534a0aba35ee0938222ad71a1ab63f4050dd80a
AhmedFathy2001 <56750728+AhmedFathy2001@users.noreply.github.com>
2022-03-24 20:26:27 +0200

    fixed bugs in the header

--- scripts/header.js
+++ scripts/header.js
@@ -1,11 +1,22 @@
 const navToggle = document.getElementById("navToggle");
 let navMenu = document.getElementById("navMenu");
-navToggle.addEventListener("click", () => {
+navToggle.addEventListener("click", (e) => {
+  e.stopPropagation();
   navMenu.classList.toggle("hidden");
   navToggle.classList.toggle("active");
   if (!navMenu.classList.contains("hidden")) {
     document.body.style.overflowY = "hidden";
   } else {
     document.body.style.overflowY = "auto";
+  }
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target != navToggle) {
+    if (e.target != navMenu) {
+      navMenu.classList.add("hidden");
+      navToggle.classList.remove("active");
+      document.body.style.overflowY = "auto";
+    }
   }
 });
